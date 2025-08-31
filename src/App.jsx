37661b680688592b25572e43a1564692da7743ba
Lemon Pecan Pie <aidanhpaul@gmail.com>
2025-08-31 10:00:19 -0400

    Finish 2.9

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [newFilter, setNewFilter] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -27,10 +28,19 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value);
+  }
+
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()));
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newFilter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -43,7 +53,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {personsToShow.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
